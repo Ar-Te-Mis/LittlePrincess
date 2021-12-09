b2bec511a6d7561fd23b32ed43641dfb5839ef06
Ar-Te-Mis <sebastianshansrs@gmail.com>
2021-12-09 23:16:34 +0700

    commit on 'master'

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+from discord.ext import commands
+
+bot = commands.Bot(
+	command_prefix="LP.",  # Change to desired prefix
+	case_insensitive=True  # Commands aren't case-sensitive
+)
+
+bot.author_id = 650331064304271370  # Change to your discord id!!!
+
+@bot.event 
+async def on_ready():  # When the bot is ready
+    print("I'm in")
+    print(bot.user)  # Prints the bot's username and identifier
+
+
+extensions = [
+	'cogs.commandmain'  # Same name as it would be if you were importing it
+]
+
+if __name__ == '__main__':  # Ensures this is the file being ran
+	for extension in extensions:
+		bot.load_extension(extension)  # Loades every extension.
+
+token = os.environ.get('TOKEN') 
+bot.run(token)  # Starts the bot
