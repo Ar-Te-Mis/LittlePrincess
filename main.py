70b1dcced5fa176cdb05da5cf3f47b480314a352
Ar-Te-Mis <sebastianshansrs@gmail.com>
2021-12-10 06:54:57 +0700

    commit on 'master'

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import os
 from discord.ext import commands
 
+
 bot = commands.Bot(
 	command_prefix="LP.",  # Change to desired prefix
-	case_insensitive=True  # Commands aren't case-sensitive
+	case_insensitive=True, # Commands aren't case-sensitive
+	help_command = None #Disable default help command  
 )
 
 bot.author_id = 650331064304271370  # Change to your discord id!!!
+
+
 
 @bot.event 
 async def on_ready():  # When the bot is ready
@@ -15,10 +19,11 @@ async def on_ready():  # When the bot is ready
 
 
 extensions = [
-	'cogs.commandmain','cogs.raidupdate'  # Same name as it would be if you were importing it
+	'cogs.commandmain','cogs.raidupdate','cogs.help'  # Same name as it would be if you were importing it
 ]
 
 if __name__ == '__main__':  # Ensures this is the file being ran
+
 	for extension in extensions:
 		bot.load_extension(extension)  # Loades every extension.
 
--- simple.py
+++ simple.py
@@ -9,56 +9,34 @@ from PIL import Image
 ocr = PaddleOCR(use_angle_cls=True, lang="en")
 
 def ResOCRToJson():
-    user_raid={}
-    result = ocr.ocr("tmp.jpg", cls=True)
+    user_raid={} #empty dictionary
+    result = ocr.ocr("tmp.jpg", cls=True) #read tmp.jpg
 
     
-    txts = [line[1][0] for line in result]
+    txts = [line[1][0] for line in result] #retrieving text in image
 
     curdex = 0
     for a in txts:
         
         approval = True
-        index = a.rfind("Lv.")
-        if a == "1":
-            approval=False
-        if index > 0:
-
-            #a[index:len(a)] = ""
-            a = a[0:index:] + a[len(a) + 1::]
-        index = a.rfind("LV.")
-        if a == "1":
-            approval=False
-        if index > 0:
-
-            
-
-            #a[index:len(a)] = ""
-            a = a[0:index:] + a[len(a) + 1::]
         
-        index = a.rfind("lv.")
-        if a == "1":
+        if len(a) == 1: #ignore rank
             approval=False
-        if index > 0:
 
-            
-
-            #a[index:len(a)] = ""
-            a = a[0:index:] + a[len(a) + 1::]
-
-        index = a.rfind("/3")
+        index = a.rfind("Lv.") * a.rfind("LV.") * a.rfind("lV.") * a.rfind("lv.") #ignore lvl
+        if index < 0:
+            a = a[0:abs(index):] + a[len(a) + 1::]
+        
+        index = a.rfind("/3") #ignore raid attemp
         if index > 0:approval = False
-        if(approval):
+        if(approval): #if only damage and user
             curdex += 1
             if curdex%2 == 1:
                 b=a
             else:
-                a = a.replace(".","")
-                print(b,a)
+                a = a.replace(".","") #replace . to none cuz of 1.232.534 is illegal to tramform to int
                 
-                user_raid[b]=int(a)
-    sorteds = dict(sorted(user_raid.items(), key=lambda item: item[1], reverse=True))
-    for i in sorteds:
-        print(i)
-    with open("DB/raid.json","w") as data_json:
+                user_raid[b]=int(a) #updating dictionary
+    sorteds = dict(sorted(user_raid.items(), key=lambda item: item[1], reverse=True)) #sort dictionary by dmg
+    with open("DB/raid.json","w") as data_json: #update the json
         json.dump(sorteds,data_json)
